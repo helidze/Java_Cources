af636f3fb1ce2e130e3ed6b8172df6c4f5cbc5da
George <helidze@gmail.com>
2017-05-29 21:36:01 +0300

    Figure

--- src/third_week/Figure/Circle.java
+++ src/third_week/Figure/Circle.java
@@ -4,21 +4,33 @@ package third_week.Figure;
  * Created by George on 29.05.2017.
  */
 public class Circle extends Figure {
+    int diametr;
+
+    public Circle(){}
+
+    public Circle(int diametr){
+        this.diametr = diametr;
+    }
+
     @Override
     public void about(){
-        setA(15);
-        System.out.println("Сторона круга равна: " + a);
+        setDiametr(20);
+        System.out.println("Диаметр круга равен: " + diametr);
     }
 
     @Override
     public void area() {
         setA(15);
-        System.out.println("Площадь круга: " + a*a);
+        System.out.println("Radius круга: " + diametr/2);
     }
 
     @Override
     public void perimeter() {
         setA(15);
         System.out.println("Периметр круга: " + (a+a));
+    }
+
+    public void setDiametr(int diametr) {
+        this.diametr = diametr;
     }
 }
