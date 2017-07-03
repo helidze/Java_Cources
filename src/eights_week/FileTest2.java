cd61134e231ca9fed32e940bb9f7274577a9e100
George <helidze@gmail.com>
2017-07-03 19:53:44 +0300

    filelesson

---
+++ src/eights_week/FileTest2.java
@@ -0,0 +1,39 @@
+package eights_week;
+
+import java.io.File;
+import java.nio.file.Files;
+
+/**
+ * Created by George on 03.07.2017.
+ */
+public class FileTest2 {
+    public static void main(String[] args) {
+
+        File directory = new File("D:\\Java_Cources\\test_dir");
+
+        File file = new File("D:\\Java_Cources\\test_dir", "file.txt");
+
+        File file1 = new File(directory, "file1.txt");
+
+        String [] files = directory.list();
+        for (String str: files
+             ) {
+            System.out.println(str);
+        }
+
+        System.out.println("List catalog: " + directory.getName());
+
+        File[] files1 = directory.listFiles();
+
+        if (files1.length>0){
+            for (File f:files1
+                 ) {
+                if (!f.isHidden())
+                System.out.println("******" + f.getName() + "*******");
+                System.out.println(f.getPath());
+                System.out.println("Parent dir: " + f.getParent());
+
+            }
+        }
+            }
+}
--- src/eights_week/File_Test.java
+++ src/eights_week/File_Test.java
@@ -1,6 +1,8 @@
 package eights_week;
 
 import java.io.File;
+import java.io.FileWriter;
+import java.io.IOException;
 
 /**
  * Created by George on 03.07.2017.
@@ -12,10 +14,35 @@ public class File_Test {
 
         File file = new File("D:\\Java_Cources\\test_dir", "file.txt");
 
-        File file1 = new File(directory,"file1.txt");
+        File file1 = new File(directory, "file1.txt");
 
         System.out.println(directory.exists());
         System.out.println(file1.isFile());
         System.out.println(directory.isDirectory());
+
+        if (file.exists() && file.isFile()) {
+            try {
+                FileWriter fileWriter = new FileWriter(file);
+                fileWriter.append("File");
+                fileWriter.flush();
+                fileWriter.close();
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+
+
+        } else {
+            try {
+                if (file1.createNewFile()) {
+                    FileWriter fileWriter1 = new FileWriter(file1);
+                    fileWriter1.append("Create new File");
+                    fileWriter1.flush();
+                    fileWriter1.close();
+                }
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        }
     }
-}
+    }
+
