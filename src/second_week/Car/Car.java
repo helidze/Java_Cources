fb6e9cc0682be6c17783dd80f4e055e26049cdb5
George <helidze@gmail.com>
2017-07-04 20:47:10 +0300

    compareTo

--- src/second_week/Car/Car.java
+++ src/second_week/Car/Car.java
@@ -3,18 +3,56 @@ package second_week.Car;
 /**
  * Created by George on 24.05.2017.
  */
-public class Car {
-    public String model;
-    protected String color;
-    public int year;
+public class Car implements Comparable<Car> {
+    private String model;
+    private String color;
+    private int year;
 
-
-
-
-    public void info(){
-        System.out.println("Model: " + model + "\n Color: " + color + "\n Year" + year);
+    public Car(String model, String color, int year) {
+        this.model = model;
+        this.color = color;
+        this.year = year;
     }
-    public static void princar(){
-        System.out.println("Car my car");
+
+//    public void info(){
+//        System.out.println("Model: " + model + "\n Color: " + color + "\n Year" + year);
+//    }
+//    public static void princar(){
+//        System.out.println("Car my car");
+//    }
+
+    public String getModel() {
+        return model;
+    }
+
+    public void setModel(String model) {
+        this.model = model;
+    }
+
+    public String getColor() {
+        return color;
+    }
+
+    public void setColor(String color) {
+        this.color = color;
+    }
+
+    public int getYear() {
+        return year;
+    }
+
+    public void setYear(int year) {
+        this.year = year;
+    }
+
+    @Override
+    public int compareTo(Car o) {
+        return this.model.compareTo(o.model);
+
+    }
+
+    @Override
+    public String toString() {
+        return model + " " + color + " " + year;
     }
 }
--- src/second_week/Car/CarTest.java
+++ src/second_week/Car/CarTest.java
@@ -1,24 +1,57 @@
 package second_week.Car;
 
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.Comparator;
+
 /**
  * Created by George on 24.05.2017.
  */
 public class CarTest {
     public static void main(String[] args) {
-        Car car1 = new Car();
-        car1.color = "Black";
-        car1.model = "Mazda3";
-        car1.year = 2017;
-        car1.info();
-        car1.princar();
 
-        System.out.println();
+        ArrayList<Car> list = new ArrayList<>();
 
-        Car car2 = new Car();
-        car2.color = "White";
-        car2.model = "Honda Accord";
-        car2.year = 2010;
-        car2.info();
-        car2.princar();
+        list.add(new Car("Model", "Black", 1989));
+        list.add(new Car("Bmw", "Red", 2011));
+        list.add(new Car("Mercedes", "Blue", 2012));
+        list.add(new Car("Honda", "Yellow", 2013));
+        list.add(new Car("Chrysler", "Grey", 2017));
+        list.add(new Car("Volkswagen", "White", 2000));
+        list.add(new Car("Daewoo", "Purple", 2020));
+
+        Collections.sort(list, new Comparator<Car>() {
+            @Override
+            public int compare(Car o1, Car o2) {
+                return o1.getModel().length() - o2.getModel().length();
+            }
+        }.thenComparing(new Comparator<Car>() {
+            @Override
+            public int compare(Car o1, Car o2) {
+                return o1.getYear() - o2.getYear();
+            }
+        }));
+
+        Collections.sort(list);
+
+        System.out.println(list);
+
+
+//        Car car1 = new Car();
+//        car1.color = "Black";
+//        car1.model = "Mazda3";
+//        car1.year = 2017;
+//        car1.info();
+//        car1.princar();
+//
+//        System.out.println();
+//
+//        Car car2 = new Car();
+//        car2.color = "White";
+//        car2.model = "Honda Accord";
+//        car2.year = 2010;
+//        car2.info();
+//        car2.princar();
+//    }
     }
 }
