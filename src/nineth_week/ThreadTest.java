51b554ffb6debb359ef215501443104d3d394183
George <helidze@gmail.com>
2017-07-10 20:19:00 +0300

    Threads

--- src/nineth_week/ThreadTest.java
+++ src/nineth_week/ThreadTest.java
@@ -5,13 +5,18 @@ package nineth_week;
  */
 public class ThreadTest {
     public static void main(String[] args) {
-        Threads threads0 = new Threads("thread0");
-        Threads threads1 = new Threads("thread1");
-        Threads threads2 = new Threads("thread2");
+        Threads threads0 = new Threads("\tthread0");
+        Threads threads1 = new Threads("\t\tthread1");
+        Threads threads2 = new Threads("\t\t\tthread2");
+
 
         threads0.start();
+        threads0.setPriority(10);
         threads1.start();
         threads2.start();
+
+
+
 
     }
 }
--- src/nineth_week/Threads.java
+++ src/nineth_week/Threads.java
@@ -15,7 +15,7 @@ public class Threads extends Thread{
     @Override
     public void run() {
         for (int i = 0; i < 10 ; i++) {
-            System.out.println(name + " " + i);
+            System.out.println(name + " " + i + getPriority() );
         }
     }
 }
