ae88cb09fe3f8e8f73c1a7f24efc1e54f921e428
George <helidze@gmail.com>
2017-07-11 19:53:49 +0300

    SecondTree

--- src/nineth_week/SecondTree/Node.java
+++ src/nineth_week/SecondTree/Node.java
@@ -4,31 +4,17 @@ package nineth_week.SecondTree;
  * Created by George on 11.07.2017.
  */
 
-import java.util.ArrayList;
-
-
 public class Node {
 
-    private String identifier;
-    private ArrayList<String> children;
+    int data;
+    Node left;
+    Node right;
 
-    // Constructor
-    public Node(String identifier) {
-        this.identifier = identifier;
-        children = new ArrayList<String>();
+
+    public Node(int data){
+        this.data = data;
+        left = null;
+        right = null;
     }
 
-    // Properties
-    public String getIdentifier() {
-        return identifier;
-    }
-
-    public ArrayList<String> getChildren() {
-        return children;
-    }
-
-    // Public interface
-    public void addChild(String identifier) {
-        children.add(identifier);
-    }
 }
---
+++ src/nineth_week/SecondTree/Tree.java
@@ -0,0 +1,47 @@
+package nineth_week.SecondTree;
+
+/**
+ * Created by George on 11.07.2017.
+ */
+public class Tree {
+    public static  Node root;
+
+    public Tree(){
+        this.root = null;
+    }
+
+    public void add(int id){
+        Node newNode = new Node(id);
+        if(root==null){
+            root = newNode;
+            return;
+        }
+        Node current = root;
+        Node parent = null;
+        while(true){
+            parent = current;
+            if(id<current.data){
+                current = current.left;
+                if(current==null){
+                    parent.left = newNode;
+                    return;
+                }
+            }else{
+                current = current.right;
+                if(current==null){
+                    parent.right = newNode;
+                    return;
+                }
+            }
+        }
+    }
+    public void display(Node root){
+        if(root!=null){
+            display(root.left);
+            System.out.print(" " + root.data);
+            display(root.right);
+        }
+    }
+
+
+}
