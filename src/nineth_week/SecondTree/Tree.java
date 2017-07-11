4138ff3a8929ab5707a7a719a15359249b03c829
George <helidze@gmail.com>
2017-07-11 21:17:12 +0300

    SecondTree

--- src/nineth_week/SecondTree/Tree.java
+++ src/nineth_week/SecondTree/Tree.java
@@ -44,4 +44,100 @@ public class Tree {
     }
 
 
+    public boolean find(int id){
+        Node current = root;
+        while(current!=null){
+            if(current.data==id){
+                return true;
+            }else if(current.data>id){
+                current = current.left;
+            }else{
+                current = current.right;
+            }
+        }
+        return false;
+    }
+    public boolean delete(int id){
+        Node parent = root;
+        Node current = root;
+        boolean isLeftChild = false;
+        while(current.data!=id){
+            parent = current;
+            if(current.data>id){
+                isLeftChild = true;
+                current = current.left;
+            }else{
+                isLeftChild = false;
+                current = current.right;
+            }
+            if(current ==null){
+                return false;
+            }
+        }
+        //if i am here that means we have found the node
+        //Case 1: if node to be deleted has no children
+        if(current.left==null && current.right==null){
+            if(current==root){
+                root = null;
+            }
+            if(isLeftChild ==true){
+                parent.left = null;
+            }else{
+                parent.right = null;
+            }
+        }
+        //Case 2 : if node to be deleted has only one child
+        else if(current.right==null){
+            if(current==root){
+                root = current.left;
+            }else if(isLeftChild){
+                parent.left = current.left;
+            }else{
+                parent.right = current.left;
+            }
+        }
+        else if(current.left==null){
+            if(current==root){
+                root = current.right;
+            }else if(isLeftChild){
+                parent.left = current.right;
+            }else{
+                parent.right = current.right;
+            }
+        }else if(current.left!=null && current.right!=null){
+
+            //now we have found the minimum element in the right sub tree
+            Node successor	 = getSuccessor(current);
+            if(current==root){
+                root = successor;
+            }else if(isLeftChild){
+                parent.left = successor;
+            }else{
+                parent.right = successor;
+            }
+            successor.left = current.left;
+        }
+        return true;
+    }
+
+    public Node getSuccessor(Node deleleNode){
+        Node successor =null;
+        Node successorParent =null;
+        Node current = deleleNode.right;
+        while(current!=null){
+            successorParent = successor;
+            successor = current;
+            current = current.left;
+        }
+        //check if successor has the right child, it cannot have left child for sure
+        // if it does have the right child, add it to the left of successorParent.
+//		successorParent
+        if(successor!=deleleNode.right){
+            successorParent.left = successor.right;
+            successor.right = deleleNode.right;
+        }
+        return successor;
+    }
+
+
 }
